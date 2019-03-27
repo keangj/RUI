1bea5a514372cfb8037e2e233d54e8c856308622
jaykang <keangjay@gmail.com>
2019-03-28 00:11:04 +0800

    <test>[<icon>]添加 icon 事件测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,17 @@
-import * as renderer from 'react-test-renderer'
-import React from 'react'
-import Icon from '../icon'
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import Icon from '../icon';
+import { mount } from 'enzyme'
 
 describe('icon', () => {
   it('icon 渲染成功', () => {
     const json = renderer.create(<Icon name="qq"/>).toJSON();
     expect(json).toMatchSnapshot();  // 在 __snapshot__ 下生成快照，用 yarn test -u 更新快照
+  });
+  it('onClick', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="qq" onClick={fn}/>);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled();
   })
 });
