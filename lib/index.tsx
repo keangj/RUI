16d389fae35ccf5227ff08f5ca3011dc6d5a0065
jaykang <keangjay@gmail.com>
2019-03-28 23:26:05 +0800

    添加配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-// import Button from './button';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
-
-ReactDOM.render(<div>
-  <Icon
-    name='qq'
-    onClick={fn}
-    onMouseEnter={()=> { console.log('mouse'); }}
-    onMouseLeave={()=> { console.log('me'); }}
-  />
-</div>, document.querySelector('#root'));
+export { default as Icon } from './icon/icon'
