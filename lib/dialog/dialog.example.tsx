75526394dadedba1c4883f4efc96497a9a89e49d
jaykang <keangjay@gmail.com>
2019-04-20 23:20:44 +0800

    [dialog]添加基本样式

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import Dialog from './dialog';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function () {
   const [x, setX] = useState(false);
   return (
     <div>
       <button onClick={() => setX(!x)}>click</button>
-      <Dialog visible={ x }/>
+      <Dialog visible={ x }>
+        你好
+      </Dialog>
     </div>
   )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,14 +1,38 @@
 import * as React from "react";
+import { Fragment } from 'react';
+import './dialog.scss'
 
 interface Props {
   visible: boolean
 }
 
+function f (pre: string) {
+  return function (name?: string) {
+    return [pre, name].filter(Boolean).join('-');
+  }
+}
+
+const localClass = f('rui-dialog');
+const lc = localClass;
+
 const Dialog: React.FunctionComponent<Props> = (props) => {
+
   return (
     <div>
       {props.visible ?
-        <div>dialog</div> :
+        <Fragment>
+          <div className={ lc('mask') }/>
+          <div className={ lc() }>
+            <header className={lc('header')}>标题</header>
+            <main className={lc('main')}>
+              { props.children }
+            </main>
+            <footer className={lc('footer')}>
+              <button>确认</button>
+              <button>取消</button>
+            </footer>
+          </div>
+        </Fragment> :
         null}
     </div>
   )
